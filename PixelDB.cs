160855cd7e4a68771336b363d032219ec275f229
Jack Casey <jackdcasey@gmail.com>
2020-05-09 21:53:16 -0700

    Commit scaffolding

---
+++ PixelDB.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PixelDB
+{
+    public class PixelDB
+    {
+
+    }
+}
---
+++ test/PixelDBTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PixelDB
+{
+    public class PixelDBTest
+    {
+        [Fact]
+        public void IsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
